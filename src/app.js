f0ad14305d51b810897227774ec185e56df80073
giangnqph15195 <giangqu13579@gmail.com>
2022-03-08 13:46:58 +0700

    buoi_1

---
+++ src/app.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var a = 20;
+var b = 30;
+var name = "Nguyen Quy Giang";
+var age = 19;
+var status = true;
+var infor = { id: age, name: name };
+var stringArr = ["a", "b", "c"];
+var numberArr = [1, 2, 3];
+var objectArr = [{ id: 1 }, { id: 2 }];
+function sum(numA, numB) {
+    return numA + numB;
+}
+console.log(sum(a, b));
+console.log(name);

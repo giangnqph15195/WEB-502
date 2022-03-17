af07ad9b802089686fb28e829021a35d289aa993
giangnqph15195 <giangqu13579@gmail.com>
2022-03-17 12:50:06 +0700

    buoi 3

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,43 +1,52 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import React,{Ro} from 'react'
 
-function App() {
-  // const products = [
-  //   {
-  //     id: 1,
-  //     name: "Product A"
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Product B"
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Product C"
-  //   }
-  // ]
-    const [status, settatus] = useState(false)
-    const [products, setProducts] = useState([
-      {
-            id: 1,
-            name: "Product A"
-          },
-          {
-            id: 2,
-            name: "Product B"
-          },
-          {
-            id: 3,
-            name: "Product C"
-          }
-    ])
+
+export default function App(props){
+  const [products, setproducts] = useState([])
+
+  useEffect(()=>{
+    const getPD = async () => {
+      const respon = await fetch("http://localhost:3001/api/products")
+      const data = await respon.json()
+      setproducts(data)
+    }
+    getPD()
+  })
+
   return (
     <div className="App">
-        <button onClick={() => settatus(!status)}>Toggled</button>
-       { status ? products.map((item,index) => <div>{item.name }</div> ): ""}
+       {products.map((item,index) => <div>{item.name }</div> )}
     </div>
+
   )
+
+  
+
 }
 
-export default App
+
+// function App() {
+//     const [status, settatus] = useState(false)
+//     const [products, setProducts] = useState([])
+//   useEffect(()=> {
+//     const getPD = async () => {
+//       const respon = await fetch("http://localhost:3001/api/products")
+//       const data = await respon.json()
+//       setProducts(data)
+//     }
+//     getPD()
+//   })
+
+//   return (
+//     <div className="App">
+//         <button onClick={() => settatus(!status)}>Toggled</button>
+//        { status ? products.map((item,index) => <div>{item.name }</div> ): ""}
+
+//     </div>
+//   )
+// }
+
+// export default App

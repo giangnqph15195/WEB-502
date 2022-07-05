19805eadac623514805fbb8d77a949aab6c755e2
giangnqph15195 <giangqu13579@gmail.com>
2022-07-05 09:53:14 +0700

    update

--- my-vue-app/src/api/products.tsx
+++ my-vue-app/src/api/products.tsx
@@ -1,12 +1,16 @@
 import { TypeProduct } from "../type/products";
 import instance from "./instance";
-
+const addressUser = () => {
+    if(localStorage.getItem('user')){
+      return JSON.parse(localStorage.getItem('user')as string).user._id
+    }
+  }
 export const list =()=> {
     const url= "/products"
     return instance.get(url)
 }
 export const create = (product: TypeProduct) => {
-    const url = "/products";
+    const url = `/products`;
     return instance.post(url, product)
 }
 export const remove = (_id: number) => {
